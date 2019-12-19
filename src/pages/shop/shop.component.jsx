f203ddb37084d031d11cfb00bcaecdc67a525875
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-12-18 17:00:58 -1000

    moved directory and shop dta inot own reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
+import CollectionPage from "../../pages/collection/collection.component";
 
 import CollectionOverView from "../../components/collections-overview/collections-overview.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionOverView />
+    <Route exact path={`${match.path}`} component={CollectionOverView} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
